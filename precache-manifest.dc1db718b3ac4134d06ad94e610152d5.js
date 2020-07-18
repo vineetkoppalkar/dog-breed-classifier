cbb6cfd6381877efc89a04bd2e701b5636b7fc48
Vineet <koppalkarvineet@gmail.com>
2020-07-18 12:44:23 -0400

    Updates

---
+++ precache-manifest.dc1db718b3ac4134d06ad94e610152d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a359e924d0a14ecea4747d0a152dd40",
+    "url": "/dog-breed-classifier/index.html"
+  },
+  {
+    "revision": "69c9a4d2068af481eaa4",
+    "url": "/dog-breed-classifier/static/css/main.fd414614.chunk.css"
+  },
+  {
+    "revision": "5131f34dfc4e8e41f962",
+    "url": "/dog-breed-classifier/static/js/2.a052e107.chunk.js"
+  },
+  {
+    "revision": "46cf1d2cb042c97af09af18efcb133b4",
+    "url": "/dog-breed-classifier/static/js/2.a052e107.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69c9a4d2068af481eaa4",
+    "url": "/dog-breed-classifier/static/js/main.3b23f598.chunk.js"
+  },
+  {
+    "revision": "2984c239817b21d4fe3a",
+    "url": "/dog-breed-classifier/static/js/runtime-main.23e0d62f.js"
+  }
+]);
